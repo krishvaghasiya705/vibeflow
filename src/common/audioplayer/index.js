4889a732cac1f7272d729402b2b4bfc978ec4448
krish vaghasiya <kvaghasiya705@gmail.com>
2025-04-09 18:43:22 +0530

    Enhance audioplayer UI with music controls and progress indicators; update trending songs layout and styles

--- src/common/audioplayer/index.js
+++ src/common/audioplayer/index.js
@@ -3,25 +3,74 @@ import "./audioplayer.scss";
 import nodataimnage from "../../assets/images/noimage.avif";
 import { NavLink } from "react-router-dom";
 import { RiFullscreenFill } from "react-icons/ri";
+import { HiOutlineSpeakerWave } from "react-icons/hi2";
+import { RxLoop, RxShuffle } from "react-icons/rx";
+import { AiFillStepBackward } from "react-icons/ai";
+import { IoMdSkipForward } from "react-icons/io";
+import { FaPlay } from "react-icons/fa";
+import { IoPauseSharp } from "react-icons/io5";
 
 export default function Audioplayer() {
   return (
     <div className="audioplayer-main">
-      <div className="audioplayer-songs-small-prev">
-        <img src={nodataimnage} alt="audioplayer-songs-banner-image" />
-        <div>
-          <h6>Song title</h6>
-          <p>
-            <NavLink to={"/"}>singers name</NavLink>,
-            <NavLink to={"/"}>singers name</NavLink>,
-            <NavLink to={"/"}>singers name</NavLink>
-          </p>
+      <div>
+        <div className="audioplayer-songs-small-prev">
+          <img src={nodataimnage} alt="audioplayer-songs-banner-image" />
+          <div>
+            <h6>Song title</h6>
+            <p>
+              <NavLink to={"/"}>singers name</NavLink>,
+              <NavLink to={"/"}>singers name</NavLink>,
+              <NavLink to={"/"}>singers name</NavLink>
+            </p>
+          </div>
         </div>
       </div>
-      <div></div>
+      <div className="audioplayer-music-control">
+        <div className="audioplayer-music-buttons">
+          <div className="audioplayer-music-suffle">
+            <RxShuffle fontSize={20} />
+          </div>
+          <div className="audioplayer-music-backward">
+            <AiFillStepBackward fontSize={20} />
+          </div>
+          <div className="audioplayer-music-play">
+            <FaPlay fontSize={20} />
+            {/* <IoPauseSharp fontSize={20} /> */}
+          </div>
+          <div className="audioplayer-music-forward">
+            <IoMdSkipForward fontSize={20} />
+          </div>
+          <div className="audioplayer-music-loop">
+            <RxLoop fontSize={20} />
+          </div>
+        </div>
+        <div className="audioplayer-music-progress-main-flx">
+          <div className="audioplayer-music-progress-time">
+            <span>0:00</span>
+          </div>
+          <div className="audioplayer-music-progress-main">
+            <div className="audioplayer-music-progress-line"></div>
+            <div className="audioplayer-music-progress-circle"></div>
+          </div>
+          <div className="audioplayer-music-time">
+            <span>5:00</span>
+          </div>
+        </div>
+      </div>
       <div className="audioplayer-sound-section">
+        <div className="audioplayer-sound-control">
+          <div className="audioplayer-sound-control-icon">
+            <HiOutlineSpeakerWave fontSize={20} />
+            {/* <HiOutlineSpeakerXMark fontSize={20} /> */}
+          </div>
+          <div className="audioplayer-sound-control-line-main">
+            <div className="audioplayer-sound-control-line-fill"></div>
+            <div className="audioplayer-sound-control-line-circle"></div>
+          </div>
+        </div>
         <div className="fullscreen-icon">
-          <RiFullscreenFill fontSize={20}/>
+          <RiFullscreenFill fontSize={20} />
           {/* <RiFullscreenExitLine fontSize={20}/> */}
         </div>
       </div>
--- src/component/homecomponents/trending/index.js
+++ src/component/homecomponents/trending/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import "./trendingsongs.scss"
+import React from "react";
+import "./trendingsongs.scss";
 
 export default function Trendingsongs() {
   return (
-    <div>
-      
+    <div className="trending-songs">
+      <h1>Trending Songs</h1>
+      <div className="trending-songs-cards">
+        <div className="trending-songs-card">
+          <div className="trending-songs-card-image">
+            <img
+              src="https://media.istockphoto.com/id/2173059563/vector/coming-soon-image-on-white-background-no-photo-available.jpg?s=612x612&w=0&k=20&c=v0a_B58wPFNDPULSiw_BmPyhSNCyrP_d17i2BPPyDTk="
+              alt="No-Data Found"
+            />
+          </div>
+          <div className="trending-songs-card-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              optio, deleniti sequi vel molestiae ducimus aut dolorum quibusdam
+              assumenda, recusandae et at. Animi, eaque? Doloribus molestiae
+              veritatis amet quam adipisci!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
